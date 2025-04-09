6a29aaa081e02ef9aafa6d3d7507aa35e65beb78
Ghostly <erwin.chavez@alumnos.ucentral.cl>
2025-04-09 10:22:55 -0400

    update

--- 02-Fundamentals-Part-2/starter/assignaments.js
+++ 02-Fundamentals-Part-2/starter/assignaments.js
@@ -1,11 +1,47 @@
 //* Functions
 
 
-function describeCountry(country, population, capitalCity) {
-  return `${country} has ${population} million people an its capital city is ${capitalCity}`
+// function describeCountry(country, population, capitalCity) {
+//   return `${country} has ${population} million people an its capital city is ${capitalCity}`
+// }
+
+// const descFinland = describeCountry('Finland', 6, 'Helsinki');
+
+// console.log(descFinland);
+
+//* Function declarations vs Expressions
+
+function percentageOfWolrd1(population) {
+  return (population / 7900) * 100;
 }
 
-const descFinland = describeCountry('Finland', 6, 'Helsinki');
+const mexicoPercentage = percentageOfWolrd1(131);
+const indiaPercentage = percentageOfWolrd1(1463);
+const brazilPercentage = percentageOfWolrd1(212);
 
-console.log(descFinland);
+console.log(mexicoPercentage, indiaPercentage, brazilPercentage);
+
+const percentageOfWolrd2 = function (population) {
+  return (population / 7900) * 100;
+}
+
+console.log(percentageOfWolrd2(131), percentageOfWolrd2(1463), percentageOfWolrd2(212));
+
+//* Arrow Functions
+
+percentageOfWolrd3 = (population) => (population / 7900) * 100;
+
+console.log(percentageOfWolrd3(131), percentageOfWolrd3(1463), percentageOfWolrd3(212));
+
+//* Function calling other functions
+
+describePopulation = (country, population) => {
+
+
+  const countryPercentage = percentageOfWolrd1(population);
+
+  return `${country} has ${population} million people, which is about ${countryPercentage} of the world`
+}
+
+console.log(describePopulation('Chile', 19));
 
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -25,14 +25,100 @@ logger();
 logger();
 
 
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice whit ${apples} apples and ${oranges} oranges`;
+
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+
+// console.log(appleJuice);
+// console.log(appleOrangeJuice);
+
+
+//* Function declarations vs expressions
+
+//! Function declaration
+
+function calcAge1(birthYear) {
+  return 2025 - birthYear;
+}
+
+const age1 = calcAge1(2000);
+
+//! Function expression
+
+const calcAge2 = function (birthYear) {
+  return 2025 - birthYear;
+}
+
+const age2 = calcAge2(2000);
+
+//! Arrow Function
+
+const calcAge3 = birthYear => 2025 - birthYear;
+
+const age3 = calcAge3(2000);
+
+console.log(age1, age2, age3);
+
+
+// const yearUntilRetirement = (birthYear, firstName) => {
+//   const age = 2025 - birthYear;
+//   const retirement = 65 - age;
+//   // return retirement;
+
+//   return `${firstName} retires in ${retirement} years`
+// }
+
+// console.log(yearUntilRetirement(2000, 'Erwin'));
+// console.log(yearUntilRetirement(2001, 'Nicolas'));
+
+
+//* Function into another function
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+
 function fruitProcessor(apples, oranges) {
-  const juice = `Juice whit ${apples} apples and ${oranges} oranges`;
+
+  const applePieces = cutFruitPieces(apples);
+  const orangesPieces = cutFruitPieces(oranges);
+  const juice = `Juice whit ${applePieces} pieces of apple and ${orangesPieces} pieces of orange`;
 
   return juice;
 }
 
-const appleJuice = fruitProcessor(5, 0);
-const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(fruitProcessor(2, 3));
 
-console.log(appleJuice);
-console.log(appleOrangeJuice);
+//* Reviewing function
+
+const calcAge = function (birthYear) {
+  return 2025 - birthYear;
+}
+
+
+const yearUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+
+  return retirement;
+
+}
+
+
+console.log(yearUntilRetirement(2000, 'Erwin'));
+console.log(yearUntilRetirement(1920, 'Nicolas'));
+
