af30841f4070b61ac777a0d91238f2334612a6a7
Ghostly <erwin.chavez@alumnos.ucentral.cl>
2025-05-13 19:28:07 -0400

    update

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,35 +1,75 @@
 'use strict';
+/*
 
 function calcAge(birthyear) {
   const age = 2025 - birthyear;
-
+  
   function printAge() {
     let output = `${firstName}, You are ${age}, born in ${birthyear}`;
     console.log(output);
-
+    
     if (birthyear >= 1981 && birthyear <= 1996) {
       var millenial = true;
       const firstName = 'Steven';
       const str = `Oh, and you're a milleanial, ${firstName}`;
       console.log(str);
-
+      
       function add(a, b) {
         return a + b;
       }
-
+      
       output = 'NEW OUTPUT!';
     }
     console.log(millenial);
     // add(2, 3);
-
+    
     console.log(output);
   }
 
   printAge();
-
+  
   return age;
 }
 
 const firstName = 'Jonas';
 
 calcAge(1994);
+
+
+//* Variables
+
+console.log(me); // Undefined
+console.log(job); // Hoisting works but in a temporal dead zone
+console.log(year); // same as job
+
+var me = 'Jonas';
+let job = 'Teacher';
+const year = 1991;
+
+//* Functions
+
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+// Example
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+*/
