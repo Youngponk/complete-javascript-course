d5180a987d0c9070a4c1ef2968ccf928a25a025f
Erwin Chávez <131835776+Youngponk@users.noreply.github.com>
2025-05-26 18:39:21 -0400

    update

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -84,11 +84,60 @@ const restaurant = {
     return t;
   },
 };
-//* Optional Chaining
 
-console.log(restaurant.openingHours.mon);
+//* Looping Objects: Objects keys, values and entries
+
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+for (const day of Object.keys(openingHours)) {
+  console.log(day);
+}
 
 /*
+//* Optional Chaining (Encadenamiento opcional)
+
+if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+console.log(restaurant.openingHours.mon?.open); // Error: can't read open
+//? WHIT optional chaining
+console.log(restaurant.openingHours?.mon?.open); // Return undefined si no encuentra mon
+
+// Example
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  // console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+//? METHODS
+
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exists');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exists');
+
+//? Arrays -> Checking if an array is empty
+
+// const users = [
+//   {
+//     name: 'Jonas',
+//     email: 'hello@jonas.io',
+//   },
+// ];
+
+const users = [];
+
+//? Optional chaining
+
+console.log(users[0]?.name ?? 'User array empty');
+
+//? WHITHOUT
+
+if (users.length > 0) console.log(users[0].name);
+else console.log('User array empty');
+
 //* Objetos literales mejorados
 
 //* FOR-OF LOOPING ARRAY
