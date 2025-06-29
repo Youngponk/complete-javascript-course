34c4c5ef8e343aee0dd81612a627505f7053f626
Ghostly <erwin.chavez@alumnos.ucentral.cl>
2025-06-29 01:06:18 -0400

    update

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1102,7 +1102,6 @@ console.log(movements);
 //? Crear un array que contenga elementos unicos que estén presentes en el array 1 y en el array 2
 
 // [...new Set(arr1).intersection(new set(arr2))]
-*/
 
 //* ------------------------------
 //* ARRAY METHOD PRACTICE        *
@@ -1158,11 +1157,67 @@ console.log(deposits, withdrawals);
 //? This is a nice title -> This Is a Nice Title
 
 const convertTitleCase = function (title) {
-  const words = title.split(' ');
-  words.map(word => word.toLowerCase());
-  console.log(words);
-  return title;
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'the', 'and', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+    .join(' ');
+  return capitalize(titleCase);
 };
-console.log(convertTitleCase('this is a nice title'));
-// console.log(convertTitleCase('this is a LONG title but no too long'));
-// console.log(convertTitleCase('and here is another title whit EXAMPLE'));
+console.log(convertTitleCase('this is a Nice title'));
+console.log(convertTitleCase('this is a LONG title but no too long'));
+console.log(convertTitleCase('and here is another title whit EXAMPLE'));
+
+
+//! ------------------------------
+//! CODING CHALLENGE 5        *
+//! ------------------------------
+
+/* 
+Julia and Kate are still studying dogs. This time they are want to figure out if the dogs in their are eating too much or too little food.
+
+- Formula for calculating recommended food portion: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+- Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+- Eating an okay amount means the dog's current food portion is within a range 10% above and below the recommended portion (see hint).
+*/
+
+const recommendedFood = function (weight) {
+  return Math.floor(weight ** 0.75 * 28);
+};
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John', 'Leo'] },
+  { weight: 18, curFood: 244, owners: ['Joe'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+//YOUR TASKS:
+//1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion (recFood) and add it to the object as a new property. Do NOT create a new array, simply loop over the array (We never did this before, so think about how you can do this without creating a new array).
+
+dogs.forEach(dog => (dog.recFood = recommendedFood(dog.weight)));
+
+console.log(dogs);
+
+//2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple users, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+
+//3. Create an array containing all owners of dogs who eat too much (ownersTooMuch) and an array with all owners of dogs who eat too little (ownersTooLittle).
+//4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+//5. Log to the console whether there is ANY dog eating EXACTLY the amount of food that is recommended (just true or false)
+//6. Log to the console whether ALL of the dogs are eating an OKAY amount of food (just true or false)
+//7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+//8. Group the dogs into the following 3 groups: 'exact', 'too-much' and 'too-little', based on whether they are eating too much, too little or the exact amount of food, based on the recommended food portion.
+//9. Group the dogs by the number of owners they have
+//10. Sort the dogs array by recommended food portion in an ascending order. Make sure to NOT mutate the original array!
+
+//HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+//HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+//TEST DATA:
+
+// GOOD LUCK 😀
